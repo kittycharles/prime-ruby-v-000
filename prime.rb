e88138a06470a6add9e51c8a9a564887063b24b9
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-27 19:53:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,12 @@
 def prime?(integer)
   if integer % 2 == false
+  elsif integer % 3 == false
+  elsif integer % 4 == false
+  elsif integer % 5 == false
+  elsif integer % 6 == false
+  elsif integer % 7 == false
+  elsif integer % 8 == false
+  elsif integer % 9 == false
   else
   true
   end
