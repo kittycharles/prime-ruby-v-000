cd30e99bc28f0e6ea9a6a46487b3d5b991f47987
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-27 20:11:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
 def prime?(integer)
+  if integer < 0
+    false
+  end
   (2..(integer - 1)).each do |n|
      if integer % n == 0
-       return false
-     elsif integer<0
        return false
      end
    end
