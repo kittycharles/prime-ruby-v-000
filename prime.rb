42a6db89c576f63a9168f6fad9273b35d7364c2f
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-27 20:15:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
-def prime?(integer)
-  integer < 1 ? true : false
-  (2..(integer - 1)).each do |n|
-     if integer % n == 0
-       return false
-     end
-   end
-   true
- end
+def prime?(number)
+  if number >= 2
+    (2..number - 1).all? do |x|
+      number % x != 0
+    end
+  else
+    return false
+  end
+end
